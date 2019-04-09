2c1a877059cdec82093fcf6216f89ab69c8c5a8d
skendall <shannonckendall@gmail.com>
2019-04-09 10:07:21 -0400

    Push for Routes Handlebars

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const exphbs = require("express-handlebars");
+const path = require('path');
 
 // Require all models
-var db = require("./models");
+let db = require("./models");
 
 // Initialize Express
-var app = express();
+const app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "index" }));
+app.engine("handlebars", exphbs({ 
+    defaultLayout: "main", 
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
 app.set("view engine", "handlebars");
 
 //Port
-var PORT = 3000;
+let PORT = 3000;
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 //Routes
@@ -31,7 +36,7 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-// A GET route for scraping the echoJS website
+// A GET route for scraping the YAHOOO website
 app.get("/scrape", function (req, res) {
     // console.log('scrapin...'); 
     // First, we grab the body of the html with axios
@@ -118,42 +123,50 @@ app.get("/articles/:id", function (req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-        .then(function (dbNote) {
-            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-        })
-        .then(function (dbArticle) {
-            // If we were able to successfully update an Article, send it back to the client
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
+    db.Note.post(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate(
+          { _id: req.params.id },
+          { note: dbNote._id },
+          { new: true }
+        );
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
 });
 
 // Route for saving/updating an Comment's associated article
-app.post("/comments/", function (req, res) {
-    // Create a new comment and pass the req.body to the entry
-    // console.log("Req Body in Comment Post: " + req.body);
-    db.Comment.create(req.body)
-        .then(function (dbNote) {
-            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.body.id }, { comment: dbNote._id }, { new: true });
-        })
-        .then(function (dbArticle) {
-            // If we were able to successfully update an Article, send it back to the client
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
+app.post("/comments/", function(req, res) {
+  // Create a new comment and pass the req.body to the entry
+  console.log("Req Body in Comment Post: " + req.body);
+  db.Comment.post(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate(
+        { _id: req.body.id },
+        { comment: dbNote._id },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
 app.listen(PORT, function () {
--- views/public/assets/js/app.js
+++ views/public/assets/js/app.js
@@ -50,15 +50,6 @@ $(document).on("click", "p", function() {
     });
 });
 
-$('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var title = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text(title)
-})
-
 // When you click the savenote button
 $(document).on("click", "#submitComment", function() {
   // Grab the id associated with the article from the submit button
