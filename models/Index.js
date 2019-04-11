5506885cd64bd517e890bc84847c88aceb48596e
skendall <shannonckendall@gmail.com>
2019-04-11 18:56:56 -0400

    added .js model exports

--- models/Index.js
+++ models/Index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 //
 module.exports = {
-  Article: require("./Article"),
-  Comment: require("./Comment")
+  Article: require("./Article.js"),
+  Comment: require("./Comment.js")
 };
